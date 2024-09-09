0cceddc8eab32fa64a3c4ef5de7b6cbaeadff49e
Sabrina Ferreira Nascimento <sabrifer@c1r4p4.42sp.org.br>
2024-09-09 19:59:00 -0300

    updated header file with new functions

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: sabrifer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 09:52:43 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/09/09 14:30:40 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/09/09 19:39:48 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,6 @@
 # include <mlx.h>
 # include <X11/keysym.h>
 # include <X11/X.h>
-
-// GET_NEXT_LINE
-char		*get_next_line(int fd);
-size_t		ft_strlen(const char *str);
-char		*ft_strdup(const char *src);
-char		*ft_strchr(const char *str, int c);
-char		*ft_strjoin(char const *s1, char const *s2);
-
-char	*ft_strrchr(const char *str, int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memset(void *dest, int c, size_t count);
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
@@ -60,5 +49,42 @@ typedef struct s_map
 	int		length;
 	int		height;
 }	t_map;
+
+// GET_NEXT_LINE
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *str);
+char		*ft_strdup(const char *src);
+char		*ft_strchr(const char *str, int c);
+char		*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_strrchr(const char *str, int c);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memset(void *dest, int c, size_t count);
+
+// FT_ARGS_CHECKLIST.C
+int		is_invalid_char(t_map *map);
+int		check_chars(t_map *map);
+int		duplicate_exit_or_start(t_map *map);
+int		is_wall(t_map *map);
+int		is_rectangular(t_map *map);
+
+// FT_ARGS_CHECK.C
+int		check_map(t_map *map);
+int		check_ber(char *str);
+
+// 	FT_INIT_MAP.C
+t_map	*populate_map_struct(char **map);
+int		count_line_infd(char *file_name);
+char	**read_map(int fd, int line_count);
+t_map	*init_map(char *file_name);
+
+// MAIN STILL
+int		is_map_valid(t_map *map);
+int		check_flood_fill(t_map *map);
+void	flood_fill(t_map *map, int x, int y);
+t_coordinates	*find_start_pos(t_map *map);
+t_coordinates	*populate_coord(char p, int x, int y);
+t_map			*ft_map_copy(t_map *map);
+
 
 #endif
