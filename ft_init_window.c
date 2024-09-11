6a45e02f7932a206d0b3ebccf1c40fd491f7102d
Sabrina Ferreira Nascimento <sabrifer@c2r5p5.42sp.org.br>
2024-09-11 13:50:52 -0300

    update to put player and coin in map

--- ft_init_window.c
+++ ft_init_window.c
@@ -6,29 +6,101 @@
 /*   By: sabrifer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 14:35:45 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/09/10 20:03:13 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/09/11 13:43:11 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-#define TILE_SIZE 32 // Each tile on the map will be 64x64 pixels
+#define TILE_SIZE 32 // each tile on the map will be 32x32 pixels
 
-// Function to draw the map
-void draw_map(mlx_t* mlx, char** map, int rows, int cols) {
-    mlx_image_t* wall = mlx_texture_to_image(mlx, mlx_load_png("./images/wall.png"));
-	mlx_image_t* ground = mlx_texture_to_image(mlx, mlx_load_png("./images/space.png"));
+/*******************************\
+typedef struct mlx_image
+{
+	const uint32_t	width;
+	const uint32_t	height;
+	uint8_t*		pixels;
+	mlx_instance_t*	instances;
+	size_t			count;
+	bool			enabled;
+	void*			context;
+}	mlx_image_t;
+\*******************************/
 
-    // Loop through the map and draw the appropriate image for each tile
-    for (int y = 0; y < rows; y++) {
-        for (int x = 0; x < cols; x++) {
-            if (map[y][x] == '1') {
-                mlx_image_to_window(mlx, wall, x * TILE_SIZE, y * TILE_SIZE);
-            } else {
-                mlx_image_to_window(mlx, ground, x * TILE_SIZE, y * TILE_SIZE);
-            }
-        }
-    }
+// they're not linked
+// create two structures first. space and wall.
+// return these structures to pass to function to initialise map
+
+// create structures for coin and player / coin, player and exit
+// they are placed on the map after the map is initialised
+
+// place exit at the end / place exit together with coin and player
+
+void	render_initial_map(mlx_t* mlx, char** map, int rows, int cols)
+{
+	mlx_image_t	*wall;
+	mlx_image_t	*floor;
+	int			i;
+	int			j;
+
+	wall = mlx_texture_to_image(mlx, mlx_load_png("./images/wall.png"));
+	floor = mlx_texture_to_image(mlx, mlx_load_png("./images/floor.png"));
+	i = 0;
+    while (i < rows)
+	{
+		j = 0;
+		while (j < cols)
+		{
+			if (map[i][j] == '1')
+				mlx_image_to_window(mlx, wall, j * TILE_SIZE, i * TILE_SIZE);
+			else
+				mlx_image_to_window(mlx, floor, j * TILE_SIZE, i * TILE_SIZE);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	place_player(mlx_t *mlx, char **map, int rows, int cols)
+{
+	mlx_image_t	*player;
+	int			i;
+	int			j;
+
+	player = mlx_texture_to_image(mlx, mlx_load_png("./images/bananacat.png"));
+	i = 0;
+	while (i < rows)
+	{
+		j = 0;
+		while (j < cols)
+		{
+			if (map[i][j] == 'P')
+				mlx_image_to_window(mlx, player, j * TILE_SIZE, i * TILE_SIZE);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	place_coin(mlx_t *mlx, char **map, int rows, int cols)
+{
+	mlx_image_t	*coin;
+	int			i;
+	int			j;
+
+	coin = mlx_texture_to_image(mlx, mlx_load_png("./images/coin.png"));
+	i = 0;
+	while (i < rows)
+	{
+		j = 0;
+		while (j < cols)
+		{
+			if (map[i][j] == 'C')
+				mlx_image_to_window(mlx, coin, j * TILE_SIZE, i * TILE_SIZE);
+			j++;
+		}
+		i++;
+	}
 }
 
 void	init_window(t_map *map)
@@ -38,12 +110,18 @@ void	init_window(t_map *map)
 	mlx_t	*mlx; // declare data struct to hold value from connection with graphycal system
 					// and store window
 
-	mlx = mlx_init(WINDOW_HEIGHT, WINDOW_WIDTH, "HELLO WORLD!", false);
+	mlx = mlx_init(WINDOW_HEIGHT, WINDOW_WIDTH, "so_long", true);
 	if (!mlx) // error check
 		printf("error\n"); // free mlx if not possible to open the window
 
 	// LOAD IMAGES/TEXTURE
-	draw_map(mlx, map -> map, map -> height, map -> length);
+	render_initial_map(mlx, map -> map, map -> height, map -> length);
+	
+	place_player(mlx, map -> map, map -> height, map -> length);	
+	place_coin(mlx, map -> map, map -> height, map -> length);	
+
+
+
 	mlx_loop(mlx);
 
 	mlx_terminate(mlx);
