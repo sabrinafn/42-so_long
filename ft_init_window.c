fd048e246f84851fd421d9dc359a37fafe68c33e
Sabrina Ferreira Nascimento <sabrifer@c2r3p5.42sp.org.br>
2024-09-13 14:38:28 -0300

    functions to move player left, right, up and down added.

--- ft_init_window.c
+++ ft_init_window.c
@@ -6,51 +6,87 @@
 /*   By: sabrifer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/10 14:35:45 by sabrifer          #+#    #+#             */
-/*   Updated: 2024/09/13 10:50:06 by sabrifer         ###   ########.fr       */
+/*   Updated: 2024/09/13 14:05:50 by sabrifer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	key_pressed_function(mlx_key_data_t keydata, void *param)
+void	move_left(t_map *map)
 {
-	(void)param;
-	/*
-	if (mlx_key_down(param, MLX_KEY_ESCAPE))
-		mlx_end_loop();
-	if (mlx_key_down(param, MLX_KEY_LEFT))
-		function_to_move_player(left);
-		
-	else if (sys == left)
-	gklgksldg
-	
-	else
-		return;
-
-	// key down: act
-	// key_hook: identify
-	*/
-	printf("tecla num: %d\n", keydata.key);
+//	move_left
+	if (map->player.value->instances->x - 32 > 0)
+		map->player.value->instances->x -= 32;
 }
 
+void	move_right(t_map *map)
+{
+//	move_right
+	//TILE_SIZE * map->length = total_length_size
+	int32_t x_total = (TILE_SIZE * map->length) - 32;
+	if (map->player.value->instances->x + 32 < x_total)
+		map->player.value->instances->x += 32;
+}
+
+void	move_up(t_map *map)
+{
+//	move_up
+	if (map->player.value->instances->y - 32 > 0)
+		map->player.value->instances->y -= 32;
+}
+
+void	move_down(t_map *map)
+{
+//	move_down
+	//TILE_SIZE * map->height = total_length_size
+	int32_t y_total = (TILE_SIZE * map->height) - 32;
+	if (map->player.value->instances->y + 32 < y_total)
+	map->player.value->instances->y += 32;
+}
+
+void	key_pressed_function(mlx_key_data_t keydata, void *param)
+{
+	t_map	*map;
+
+	map = param;
+	//	close window with esc
+	if (mlx_is_key_down(map->mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(map->mlx);
+	//	move_left
+	if (mlx_is_key_down(map->mlx, MLX_KEY_LEFT))
+		move_left(map);
+	//	move_right
+	if (mlx_is_key_down(map->mlx, MLX_KEY_RIGHT))
+		move_right(map);
+	//	move_up
+	if (mlx_is_key_down(map->mlx, MLX_KEY_UP))
+		move_up(map);
+	//	move_down
+	if (mlx_is_key_down(map->mlx, MLX_KEY_DOWN))
+		move_down(map);
+	
+	// key down: act
+	// key_hook: identify
+	printf("tecla num: %d\n", keydata.key);
+}
 
 void	init_window(t_map *map)
 {
 	// DECLARE DATA AND OPEN WINDOW
-	mlx_t	*mlx; // declare data struct to hold value from connection with graphycal system
+//	mlx_t	*mlx; // declare data struct to hold value from connection with graphycal system
 					// and store window
-	mlx = mlx_init(WINDOW_HEIGHT, WINDOW_WIDTH, "so_long", true);
-	if (!mlx) // error check
+	map->mlx = mlx_init(WINDOW_HEIGHT, WINDOW_WIDTH, "so_long", true);
+	if (!map->mlx) // error check
 		printf("error\n"); // free mlx if not possible to open the window
 
 	// LOAD IMAGES/TEXTURE
-	render_initial_map(mlx, map -> map, map -> height, map -> length);
+	render_initial_map(map->mlx, map -> map, map -> height, map -> length);
 
-	place_player(mlx, map -> map, map -> height, map -> length);
-	place_coin(mlx, map -> map, map -> height, map -> length);
-	place_exit(mlx, map -> map, map -> height, map -> length);
+	place_player(map->mlx, map);//map -> map, map -> height, map -> length);
+	place_coin(map->mlx, map -> map, map -> height, map -> length);
+	place_exit(map->mlx, map -> map, map -> height, map -> length);
 
-	mlx_key_hook(mlx, &key_pressed_function, mlx);
-	mlx_loop(mlx);
-	mlx_terminate(mlx); // mlx_end_loop
+	mlx_key_hook(map->mlx, &key_pressed_function, map);
+	mlx_loop(map->mlx);
+	mlx_terminate(map->mlx); // mlx_end_loop
 }
